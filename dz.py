f880cefaa0646cce82c6a6e13046955bf1d2bece
Dima0913 <dimarovno2009@gmail.com>
2024-01-02 13:44:05 +0200

    first commit

---
+++ dz.py
@@ -0,0 +1,40 @@
+# Ex1
+# text = input("enter text : ")
+# isPalindrom = False
+# rev = text[::-1]
+# num = 0
+# for i in text:
+#     if i == rev[num]:
+#         isPalindrom = True
+#     else:
+#         isPalindrom =False
+#     num += 1
+# if(isPalindrom):
+#     print("Palindrom")
+
+
+# Ex3
+# text  = input("Enter text : ")
+# count = 0
+# for i in text:
+#     if i == ".":
+#         count += 1 
+#     elif i == "!":
+#         count += 1
+#     elif i == '?':
+#         count += 1
+# print(count)   
+
+# Ex2
+# text = input("Enter some text: ")
+# words = []
+
+# word = input("Enter word: ")
+# while(word != "0"):
+#     words.append(word)
+#     word = input("Enter word or enter 0 to stop: ")
+
+# for word in words:
+#         text = text.replace(word, word.upper())
+        
+# print(text)
